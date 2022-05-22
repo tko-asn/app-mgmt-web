260be3716a064046d6e4dcbf3d12b1e9bc448701
tko-asn <lssbfp26@icloud.com>
2022-05-22 23:52:59 +0900

    for~of文を使用できるようにeslintの設定を変更 #5

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,11 +20,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'import',
-  ],
+  plugins: ['react', '@typescript-eslint', 'import'],
   rules: {
     'react/function-component-definition': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.jsx', 'tsx'] }],
@@ -37,10 +33,13 @@ module.exports = {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
     'object-curly-newline': 'off',
+    'operator-linebreak': 'off',
     'import/prefer-default-export': 'off',
     'arrow-body-style': 'off',
     'no-use-before-define': 'off',
     'linebreak-style': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'react/jsx-curly-newline': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -49,6 +48,24 @@ module.exports = {
         jsx: 'never',
         ts: 'never',
         tsx: 'never',
+      },
+    ],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
       },
     ],
   },
