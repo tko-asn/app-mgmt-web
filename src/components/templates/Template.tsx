25d4a26f2e8fe045f6121e74eae6f7ae12e259bd
tko-asn <lssbfp26@icloud.com>
2022-06-18 01:14:42 +0900

    メッセージ機能を表示できるように仕様を変更

--- src/components/templates/Template.tsx
+++ src/components/templates/Template.tsx
@@ -1,13 +1,26 @@
 import type { VFC } from 'react';
+import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 import Header, { HeaderProps } from '../organisms/Header';
 import { useProfile } from '../../contexts/ProfileContext';
+import Message from '../atoms/Message';
 
 export type TemplateProps = {
   children: JSX.Element;
+  messages?: {
+    errors: string[];
+    info: string[];
+  };
+  deleteErrorMessages?: () => void;
+  deleteInfoMessages?: () => void;
 };
 
-const Template: VFC<TemplateProps> = ({ children }) => {
+const Template: VFC<TemplateProps> = ({
+  children,
+  messages,
+  deleteErrorMessages,
+  deleteInfoMessages,
+}) => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   const { profile } = useProfile();
 
@@ -19,7 +32,7 @@ const Template: VFC<TemplateProps> = ({ children }) => {
   if (!isAuthenticated) {
     headerProps.menus.unshift({
       name: 'ログイン',
-      handleClick: () => loginWithRedirect(),
+      handleClick: loginWithRedirect,
     });
   } else {
     headerProps.menus = [
@@ -28,7 +41,8 @@ const Template: VFC<TemplateProps> = ({ children }) => {
       { name: 'チーム作成', to: '/create/team' },
       {
         name: 'ログアウト',
-        handleClick: () => logout({ returnTo: process.env.REACT_APP_AUTH0_CALLBACK_URL }),
+        handleClick: () =>
+          logout({ returnTo: process.env.REACT_APP_AUTH0_CALLBACK_URL }),
       },
     ];
   }
@@ -36,9 +50,34 @@ const Template: VFC<TemplateProps> = ({ children }) => {
   return (
     <>
       <Header {...headerProps} />
+      <StyledMessageContainer>
+        {messages && messages.errors.length > 0 && (
+          <Message
+            messages={messages.errors}
+            handleClick={deleteErrorMessages || (() => {})}
+          />
+        )}
+        {messages && messages.info.length > 0 && (
+          <Message
+            background="#00bfff"
+            messages={messages.info}
+            handleClick={deleteInfoMessages || (() => {})}
+          />
+        )}
+      </StyledMessageContainer>
       {children}
     </>
   );
 };
+
+const StyledMessageContainer = styled.div`
+  position: fixed;
+  top: 36px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 98%;
+  z-index: 999;
+`;
 
 export default Template;
