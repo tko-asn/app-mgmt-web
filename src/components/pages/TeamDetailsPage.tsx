b756ff9454984e32483a8486ad9a12790b8bd3c6
tko-asn <lssbfp26@icloud.com>
2022-06-18 01:16:51 +0900

    変数をメモ化

--- src/components/pages/TeamDetailsPage.tsx
+++ src/components/pages/TeamDetailsPage.tsx
@@ -1,11 +1,19 @@
 import { useLazyQuery, useMutation } from '@apollo/client';
-import { useEffect, useState, VFC } from 'react';
+import { useMemo, useEffect, VFC } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { useProfile } from '../../contexts/ProfileContext';
-import { FETCH_TEAM, ADD_MEMBERS_TO_TEAM, DELETE_INVITEES_FROM_TEAM, DELETE_MEMBERS_FROM_TEAM } from '../../queries/team';
+import { useMessages } from '../../hooks/useMessages';
+import {
+  FETCH_TEAM,
+  ADD_MEMBERS_TO_TEAM,
+  DELETE_INVITEES_FROM_TEAM,
+  DELETE_MEMBERS_FROM_TEAM,
+} from '../../queries/team';
 import { getDatetime } from '../../utils/app';
-import { Team } from '../../utils/types';
+import { ACCEPT, DELETE } from '../../utils/colors';
+import { NO_TEAM_MEMBERS, TEAM_NOT_FOUND } from '../../utils/errors';
+import { Profile } from '../../utils/types';
 import Button from '../atoms/Button';
 import Padding from '../atoms/Padding';
 import Text from '../atoms/Text';
@@ -15,16 +23,7 @@ import Template from '../templates/Template';
 import { baseCardObj, DEFAULT_SIZE } from './props/detailsPage';
 
 const TeamDetailsPage: VFC = () => {
-  const initialState = {
-    teamName: '',
-    description: '',
-    invitees: [],
-    members: [],
-    createdAt: '',
-    updatedAt: '',
-  };
-  const [team, setTeam] = useState<Team>(initialState);
-  const [fetchTeam, { data }] = useLazyQuery(FETCH_TEAM);
+  const [fetchTeam, { data, loading, error: fetchTeamError }] = useLazyQuery(FETCH_TEAM);
   const [addMembersToTeam] = useMutation(ADD_MEMBERS_TO_TEAM);
   const [deleteMembersFromTeam] = useMutation(DELETE_MEMBERS_FROM_TEAM);
   const [deleteInviteesFromTeam] = useMutation(DELETE_INVITEES_FROM_TEAM);
@@ -33,76 +32,113 @@ const TeamDetailsPage: VFC = () => {
     profile: { id },
   } = useProfile();
   const navigate = useNavigate();
+  const { messages, addErrorMessage, initializeErrorMessages } = useMessages();
 
-  const cardList = [
-    {
-      ...baseCardObj,
-      subTextValue: team.description,
-      textValue: '説明',
-    },
-    {
-      ...baseCardObj,
-      subTextValue: getDatetime(team.createdAt),
-      textValue: '作成日',
-    },
-    {
-      ...baseCardObj,
-      subTextValue: getDatetime(team.updatedAt),
-      textValue: '最終更新日',
-    },
-  ];
-
-  const fetchAndSetTeam = async () => {
-    const variables = { id: teamId };
-    const { data: { getTeamById: result } } = await fetchTeam({ variables });
-    setTeam({ ...team, ...result });
-  };
+  const cardList = useMemo(
+    () => [
+      {
+        ...baseCardObj,
+        subTextValue: data?.getTeamById.description,
+        textValue: '説明',
+      },
+      {
+        ...baseCardObj,
+        subTextValue: getDatetime(data?.getTeamById.createdAt),
+        textValue: '作成日',
+      },
+      {
+        ...baseCardObj,
+        subTextValue: getDatetime(data?.getTeamById.updatedAt),
+        textValue: '最終更新日',
+      },
+    ],
+    [data?.getTeamById],
+  );
 
   useEffect(() => {
-    fetchAndSetTeam();
+    if (fetchTeamError) {
+      if (fetchTeamError.message === TEAM_NOT_FOUND) {
+        navigate('/error/notFound');
+      } else {
+        navigate('/error');
+      }
+    }
+  }, [fetchTeamError]);
+
+  useEffect(() => {
+    fetchTeam({ variables: { id: teamId } });
   }, [teamId]);
 
-  const memberCards = team.members.map((user) => ({ user }));
-  const inviteeCards = team.invitees.map((user) => ({ user }));
+  const memberCards = useMemo(
+    () =>
+      (data?.getTeamById.members
+        ? data?.getTeamById.members.map((user: Profile) => ({ user }))
+        : []),
+    [data?.getTeamById.members],
+  );
+  const inviteeCards = useMemo(
+    () =>
+      (data?.getTeamById.invitees
+        ? data?.getTeamById.invitees.map((user: Profile) => ({ user }))
+        : []),
+    [data?.getTeamById.invitees],
+  );
 
   const isYourTeam = () => {
-    const members = team.members.map((member) => member.id);
+    const members = data?.getTeamById.members
+      ? data?.getTeamById.members.map((member: Profile) => member.id)
+      : [];
     return members.includes(id);
   };
 
   const isYourInviter = () => {
-    const inviteeIds = team.invitees.map((invitee) => invitee.id);
+    const inviteeIds = data?.getTeamById.invitees
+      ? data?.getTeamById.invitees.map((invitee: Profile) => invitee.id)
+      : [];
     return inviteeIds.includes(id);
   };
 
   const variablesForTeamEditing = { id: teamId, profileIds: [id] };
 
   const leaveTeam = async () => {
-    await deleteMembersFromTeam({ variables: variablesForTeamEditing });
-    await fetchAndSetTeam();
+    await deleteMembersFromTeam({ variables: variablesForTeamEditing }).catch(
+      (err) => {
+        if (err.message === NO_TEAM_MEMBERS) {
+          addErrorMessage('チームには最低でも1名のメンバーが必要です。');
+        }
+      },
+    );
+    await fetchTeam({ variables: { id: teamId } });
   };
 
   const joinTeam = async () => {
     await addMembersToTeam({ variables: variablesForTeamEditing });
-    await fetchAndSetTeam();
+    await fetchTeam({ variables: { id: teamId } });
   };
 
   const rejectInvitation = async () => {
     await deleteInviteesFromTeam({ variables: variablesForTeamEditing });
-    await fetchAndSetTeam();
+    await fetchTeam({ variables: { id: teamId } });
   };
 
   return (
-    <Template>
+    <Template messages={messages} deleteErrorMessages={initializeErrorMessages}>
       <StyledContainer>
-        {data && (
+        {data && !loading && (
           <>
             <Padding bottom="25px" left="0px" right="0px">
-              <Text size="1.5em" value={team.teamName} weight="bold" />
+              <Text
+                size="1.5em"
+                value={data?.getTeamById.teamName}
+                weight="bold"
+              />
               {isYourTeam() && (
                 <Padding left="0px" right="0px">
                   <Button
+                    fontSize="0.9em"
                     handleClick={() => navigate(`/edit/team/${teamId}`)}
+                    height="30px"
+                    width="110px"
                     title="チームを編集"
                   />
                 </Padding>
@@ -110,21 +146,25 @@ const TeamDetailsPage: VFC = () => {
             </Padding>
             <div>
               <Text size={DEFAULT_SIZE} value="メンバー" weight="bold" />
-              <Padding bottom="20px" left="0px" right="0px" top="0px">
+              <Padding left="0px" right="0px" top="0px">
                 <UserCardList
                   cards={memberCards}
                   noCardText="メンバーが存在しません"
                 />
-                {isYourTeam() && (
-                  <Padding left="0px" right="0px">
-                    <Button
-                      background="#DD0000"
-                      handleClick={() => leaveTeam()}
-                      title="チームを抜ける"
-                    />
-                  </Padding>
-                )}
               </Padding>
+              {isYourTeam() && (
+                <StyledButtonWrapper>
+                  <Button
+                    background={DELETE}
+                    fontSize="0.9em"
+                    handleClick={() => leaveTeam()}
+                    height="30px"
+                    title="チームを抜ける"
+                    width="130px"
+                    disabled={data?.getTeamById.members.length === 1}
+                  />
+                </StyledButtonWrapper>
+              )}
             </div>
             <div>
               <Text
@@ -141,17 +181,21 @@ const TeamDetailsPage: VFC = () => {
               {isYourInviter() && (
                 <StyledButtonWrapper>
                   <Button
-                    background="#DD0000"
+                    background={DELETE}
+                    fontSize="0.9em"
                     handleClick={() => rejectInvitation()}
                     title="招待を拒否"
-                    width="45%"
+                    width="100px"
                   />
-                  <Button
-                    background="#0099FF"
-                    handleClick={() => joinTeam()}
-                    title="チームに参加"
-                    width="45%"
-                  />
+                  <Padding top="0px" right="0px" bottom="0px" left="15px">
+                    <Button
+                      background={ACCEPT}
+                      fontSize="0.9em"
+                      handleClick={() => joinTeam()}
+                      title="チームに参加"
+                      width="120px"
+                    />
+                  </Padding>
                 </StyledButtonWrapper>
               )}
             </div>
@@ -171,8 +215,8 @@ const StyledContainer = styled.div`
 
 const StyledButtonWrapper = styled.div`
   display: flex;
-  justify-content: space-around;
-  margin-bottom: 20px;
+  justify-content: flex-end;
+  margin-bottom: 10px;
 `;
 
 export default TeamDetailsPage;
--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,69 +1,63 @@
 import { useLazyQuery } from '@apollo/client';
-import { useEffect, useState, VFC } from 'react';
+import { useEffect, useLayoutEffect, useMemo, VFC } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { useProfile } from '../../contexts/ProfileContext';
 import { FETCH_SVCS, FETCH_SVCS_BY_PROFILE_ID } from '../../queries/svc';
 import { createAppList } from '../../utils/app';
-import { Apps, SimpleTeam } from '../../utils/types';
+import { SimpleTeam } from '../../utils/types';
 import AppList from '../molecules/AppList';
 import SideMenu from '../organisms/SideMenu';
 import Template from '../templates/Template';
 import { FETCH_TEAMS_BY_MEMBER_ID } from '../../queries/team';
 import TeamLinkList from '../organisms/TeamLinkList';
+import { SIDE_MENU_ACTIVEBG, SIDE_MENU_LINK } from '../../utils/colors';
 
 const TopPage: VFC = () => {
-  const initialState = {
-    latestApps: [],
-    myApps: [],
-    myTeams: [],
-  };
-
-  type TopDataState = {
-    latestApps: Apps;
-    myApps: Apps;
-    myTeams: SimpleTeam[];
-  };
-  const [topData, setTopData] = useState<TopDataState>(initialState);
-  const [fetchSvcs] = useLazyQuery(FETCH_SVCS);
-  const [fetchTeamsByMemberId] = useLazyQuery(FETCH_TEAMS_BY_MEMBER_ID);
+  const [fetchSvcs, { data: latestAppData }] = useLazyQuery(FETCH_SVCS);
+  const [fetchTeamsByMemberId, { data: myTeamData }] = useLazyQuery(FETCH_TEAMS_BY_MEMBER_ID);
   const { profile } = useProfile();
-  const [fetchSvcsByProfileId] = useLazyQuery(FETCH_SVCS_BY_PROFILE_ID);
+  const [fetchSvcsByProfileId, { data: myAppData }] = useLazyQuery(FETCH_SVCS_BY_PROFILE_ID);
   const navigate = useNavigate();
   const { isAuthenticated } = useAuth0();
 
-  const latestAppList = createAppList(topData.latestApps, navigate);
-  const myAppList = createAppList(topData.myApps, navigate);
+  const latestAppList = useMemo(() => (latestAppData?.getSvcs
+    ? createAppList(latestAppData?.getSvcs.svcs, navigate)
+    : []), [latestAppData?.getSvcs.svcs]);
+  const myAppList = useMemo(() => (myAppData?.getSvcsByProfileId
+    ? createAppList(myAppData?.getSvcsByProfileId.svcs, navigate)
+    : []), [myAppData?.getSvcsByProfileId.svcs]);
 
-  const hyperLinkListProps = {
-    activebg: '#244383',
-    menuList: [{ name: 'ホーム', to: '/top' }],
-  };
+  const baseMenuList = [{ name: 'ホーム', to: '/top', color: SIDE_MENU_LINK }];
+  const additionalMenus = [
+    { name: '自分のアプリ', to: '/top/myapp', color: SIDE_MENU_LINK },
+    { name: '自分のチーム', to: '/top/myteam', color: SIDE_MENU_LINK },
+  ];
 
-  const getTeamMenu = (teams: SimpleTeam[]) => {
-    return teams.map((team: SimpleTeam) => ({
+  const hyperLinkListProps = useMemo(() => ({
+    activebg: SIDE_MENU_ACTIVEBG,
+    menuList: isAuthenticated
+      ? baseMenuList.concat(additionalMenus)
+      : baseMenuList,
+  }), [isAuthenticated]);
+
+  const teams = useMemo(() => (myTeamData?.getTeamsByMemberId
+    ? myTeamData?.getTeamsByMemberId.teams.map((team: SimpleTeam) => ({
       color: '#000',
       name: team.teamName,
       handleClick: () => navigate(`/team/${team.id}`),
       height: '40px',
-    }));
-  };
+    })) : []), [myTeamData]);
 
-  if (isAuthenticated) {
-    const additionalMenus = [
-      { name: '自分のアプリ', to: '/top/myapp' },
-      { name: '自分のチーム', to: '/top/myteam' },
-    ];
-    hyperLinkListProps.menuList = (
-      hyperLinkListProps.menuList.concat(additionalMenus));
-  }
+  useLayoutEffect(() => {
+    fetchSvcs({ variables: { page: 1 } });
+  }, []);
 
   useEffect(() => {
     const fetchApps = async () => {
-      const newTopData = { ...topData };
       if (isAuthenticated && profile.id) {
-        const values = await Promise.all([
+        await Promise.all([
           fetchSvcsByProfileId({
             variables: { page: 1, profileId: profile.id },
           }),
@@ -71,15 +65,7 @@ const TopPage: VFC = () => {
             variables: { page: 1, memberId: profile.id },
           }),
         ]);
-        newTopData.myApps = values[0].data.getSvcsByProfileId.svcs;
-        newTopData.myTeams = values[1].data.getTeamsByMemberId.teams;
       }
-      const {
-        data: {
-          getSvcs: { svcs: latestApps },
-        },
-      } = await fetchSvcs({ variables: { page: 1 } });
-      setTopData({ ...newTopData, latestApps }); // 一括で保存
     };
     fetchApps();
   }, [isAuthenticated, profile.id]);
@@ -88,7 +74,7 @@ const TopPage: VFC = () => {
     <Template>
       <StyledContainer>
         <StyledWrapper>
-          <SideMenu width="25%" hyperLinkListProps={hyperLinkListProps} />
+          <SideMenu width="25%" maxWidth="250px" hyperLinkListProps={hyperLinkListProps} />
           <StyledContentContainer>
             <StyledContentWrapper>
               <Routes>
@@ -101,7 +87,7 @@ const TopPage: VFC = () => {
                   path="/myteam"
                   element={(
                     <TeamLinkList
-                      menuList={getTeamMenu(topData.myTeams)}
+                      menuList={teams}
                       noTeamText="所属しているチームがありません"
                     />
                   )}
