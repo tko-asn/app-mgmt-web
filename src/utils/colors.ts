f900b34cc7b552f26f5042b5ef23a5ef5b6a6ad1
tko-asn <lssbfp26@icloud.com>
2022-06-18 00:42:40 +0900

    CSSの色の定数を作成

---
+++ src/utils/colors.ts
@@ -0,0 +1,9 @@
+export const SIDE_MENU_LINK = '#666666';
+
+export const SIDE_MENU_ACTIVEBG = '#F5F5F5';
+
+export const DELETE = '#DD0000';
+
+export const ACCEPT = '#0099FF';
+
+export const CANCEL = '#b3b3b3';
