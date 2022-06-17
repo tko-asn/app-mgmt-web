e4af84fcf9ad35627b777b4c067e59a9dbc1a86f
tko-asn <lssbfp26@icloud.com>
2022-06-18 00:43:11 +0900

    APIからのエラーメッセージの定数を作成

---
+++ src/utils/errors.ts
@@ -0,0 +1,9 @@
+export const NO_TEAM_MEMBERS = 'Team needs at least one member';
+
+export const SVC_NOT_FOUND = 'Could not find service';
+
+export const TEAM_NOT_FOUND = 'Could not find team';
+
+export const PROFILE_NOT_FOUND = 'Could not find profile';
+
+export const USER_ALREADY_EXISTS = 'This user already exists';
